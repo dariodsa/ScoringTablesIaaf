a7afb5042e794def6be3f888b62dfd118ebacf28
Dario Sindicic <dario.sindicic@gmail.com>
2018-07-13 21:59:08 +0200

    pom-xml changes

---
+++ src/main/java/hr/fer/akmaksimir/Application.java
@@ -0,0 +1,25 @@
+package hr.fer.akmaksimir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class is a starting point of a spring application.
+ * 
+ * @author Josip Nimac
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Method serves as an entry point into the program.
+     * 
+     * @param args
+     *            - command line arguments that should be left empty
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
