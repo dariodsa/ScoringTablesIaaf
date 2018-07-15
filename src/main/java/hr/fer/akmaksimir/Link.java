39dbe4d36646b4f447567ed0ac5a240aa30aa413
Dario Sindicic <dario.sindicic@gmail.com>
2018-07-15 21:22:10 +0200

    static class with links

---
+++ src/main/java/hr/fer/akmaksimir/Link.java
@@ -0,0 +1,21 @@
+package hr.fer.akmaksimir;
+
+public class Link {
+	
+	/**
+	 * list of genders
+	 */
+	public static final String listGender = "/api/listGenders";
+	/**
+	 * list of categories
+	 */
+	public static final String listCategories = "/api/listCategories";
+	/**
+	 * list of measurement
+	 */
+	public static final String listMeasurment = "/api/listMeasurment";
+	/**
+	 * list of disciplines
+	 */
+	public static final String listDisciplines = "/api/listDisciplines";
+}
