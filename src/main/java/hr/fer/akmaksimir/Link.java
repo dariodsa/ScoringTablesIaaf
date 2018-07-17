2efaba874a7482cc693907364cb9287a78e9749d
Dario Sindicic <dario.sindicic@gmail.com>
2018-07-17 10:42:41 +0200

    new links

--- src/main/java/hr/fer/akmaksimir/Link.java
+++ src/main/java/hr/fer/akmaksimir/Link.java
@@ -1,23 +1,35 @@
 package hr.fer.akmaksimir;
 
+/**
+ * Collection of links used for API. 
+ * @author dario
+ *
+ */
 public class Link {
 	
 	/**
 	 * list of genders
 	 */
 	public static final String listGender = "/api/listGenders";
+	
 	/**
 	 * list of categories
 	 */
 	public static final String listCategories = "/api/listCategories";
+	
 	/**
 	 * list of measurement
 	 */
 	public static final String listMeasurment = "/api/listMeasurment";
+	
 	/**
 	 * list of disciplines
 	 */
 	public static final String listDisciplines = "/api/listDisciplines";
 	
 	public static final String competitions = "/api/competitions";
+	
+	public static final String getPoints = "/api/getPoints";
+	
+	public static final String result = "/api/result";
 }
