167b4797959849ec3280440528dd42d1fd2c936e
Dario Sindicic <dario.sindicic@gmail.com>
2018-07-20 22:44:23 +0200

    User entity

---
+++ iaaf-backend/src/main/java/hr/fer/akmaksimir/model/User.java
@@ -0,0 +1,53 @@
+package hr.fer.akmaksimir.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table ( name = "User")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column
+	@Size(min = 3, max = 15)
+	private String username;
+	
+	@Column
+	private String passwordHash;
+
+	public User() {
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+	
+	
+}
