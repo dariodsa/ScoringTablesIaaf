e5d27ee61e8f99c114915861431dc217b0f59cf1
Dario Sindicic <dario.sindicic@gmail.com>
2018-07-20 23:25:26 +0200

    logout and session saving

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,8 +13,15 @@ import { RestConstants } from './service/RestConstants';
 export class AppComponent{
   title = 'Aplikacija za računanje IAAF bodova';
   author = "Dario Sindičić, AK Maksimir, 2018."
+
+  private loginIn : boolean = false;
+  private username : string = "";
+
   constructor(private restService : SimpleRestApiService) {
-    
+    if(sessionStorage.getItem("userId") != null && sessionStorage.getItem("userId").length > 0) {
+      this.loginIn = true;
+      this.username = sessionStorage.getItem("username");
+    }
   }
   login() {
     
@@ -26,13 +33,22 @@ export class AppComponent{
                       password : password}).subscribe(
                     (res : number) => {
                       if(res != -1) {
-                        var date = new Date();
-                        date.setTime(date.getTime() + (1 * 24 * 60 * 60 * 1000));
-                        document.cookie = "userId=" + res + ";expires=" + date.toUTCString + ";";
+                        sessionStorage.setItem("userId", res.toString());
+                        sessionStorage.setItem("username", username);
+
+                        this.loginIn = true;
+                        this.username = sessionStorage.getItem("username");
+
                       } else {
                         alert("Login nije uspio");
                       }
                     }
     );
+  }
+  logout() {
+    sessionStorage.setItem("userId", null);
+    sessionStorage.setItem("username", null);
+    this.loginIn = false;
+    this.username = null;
   }
 }
